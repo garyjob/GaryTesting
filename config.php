a6e900066b4b621f94fd71b3c6d0e414b450cd8d
Gary Teh <garyjob@gmail.com>
2011-09-28 18:37:35 +0800

    testing upload again

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+/*
+	28th September 2011
+	Gary created this file to contain configuration information on the disconnectDB system.
+	This will help make the system easily portable from servers to servers
+*/
+
+?>
